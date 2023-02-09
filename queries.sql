c0807390da68f273d2db98b5620bf3c3a4cb73f4
Katrina Augustine <katrinaaugustine2022@gmail.com>
2023-02-09 16:40:57 -0600

    Solved a few of the problems, working on others

--- queries.sql
+++ queries.sql
@@ -1,20 +1,48 @@
 -- Problem 1 & 2
 -- 1. Using JOINs in a single query, combine data from all three tables (employees, products, sales) to view all sales with complete employee and product information in one table.
--- SELECT * FROM employees
+
+-- SELECT FirstName, MiddleInitial, LastName, Name, Price, Quantity 
+-- FROM employees 
+-- Inner Join sales  
+-- ON employees.EmployeeID = sales.SalesPersonID
+-- Inner Join products
+-- ON sales.SalesID = products.ProductID;
+
 -- 2a. Create a View for the query you made in Problem 1 named "all_sales"
+
+-- CREATE VIEW All_Sales AS 
+-- SELECT FirstName, MiddleInitial, LastName, Name, Price, Quantity
+-- employees, products, sales
+-- WHERE employees.EmployeeID = SalesPersonID AND
+-- sales.SalesID = products.ProductID;
+
 -- NOTE: You'll want to remove any duplicate columns to clean up your view!
 
-
 -- 2b. Test your View by selecting all rows and columns from the View
+
+-- SELECT * FROM All_Sales;
 
 -- Problem 3
 -- Find the average sale amount for each sales person
 
+-- SELECT FirstName, LastName, SalesID, Quantity, Price, (Quantity * Price) AS Extended_Price
+-- FROM Employees
+-- Join Sales
+-- ON employees.EmployeeID = sales.SalesPersonID 
+-- JOIN Products 
+-- ON products.ProductID = sales.ProductID
+-- ORDER BY FirstName ASC
+
 -- Problem 4
 -- Find the top three sales persons by total sales
 
+
 -- Problem 5
 -- Find the product that has the highest price
+
+-- SELECT Name, Price 
+-- FROM products 
+-- ORDER BY Price DESC
 
 -- Problem 6
 -- Find the product that was sold the most times
